32a17e74e39cf15917b2514f597f671fe49fa0d3
mmolski <mikolajm2001@gmail.com>
2025-05-15 18:52:47 +0200

    Adding more gam models to the function

--- R/fit_gam.R
+++ R/fit_gam.R
@@ -1,17 +1,27 @@
-#' Function for modelling fMRI data
+#' Fitting the GAMs
 #'
-#' @param data_frame A processed data frame
-#' @param smoothing_basis A smoothing function used for modelling
-#' @param number_of_knots A number of knots used for local smoothing
-#' @param method A method of estimation
+#' @param data_con_1 Data frame for condition 1
+#' @param data_con_2 Data frame for condition 2
+#' @param data_comb Data frame for combined conditions
+#' @param smoothing_basis Smooth term
+#' @param number_of_knots Basis dimension choice for smooths
+#' @param estimating_method The smoothing parameter estimation method
 #'
-#' @returns A model
+#' @returns List of 3 models.
 #' @export
-fit_gam <- \(data_frame, smoothing_basis = "tp", number_of_knots = 10, estimating_method = "REML"){
+#'
+fit_gam <- \(data_con_1, data_con_2, data_comb, smoothing_basis = "tp", number_of_knots = 10, estimating_method = "REML"){
 
-  g <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots, by = condition)
-      + s(base_values, bs = bs_options[i], k = number_of_knots, by = condition)
+  gam_1 <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
+      + ti(Time_to_Repetition, base_values), data = data_con_1, method = estimating_method, select = TRUE)
+
+  gam_2 <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
+      + ti(Time_to_Repetition, base_values), data = data_con_2, method = estimating_method, select = TRUE)
+
+  gam_comb <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots, by = condition)
+      + s(base_values, bs = smoothing_basis, k = number_of_knots, by = condition)
       + ti(Time_to_Repetition, base_values) + condition,
-      data = data_frame, method = estimating_method, select = TRUE)
-  return(g)
+      data = data_comb, method = estimating_method, select = TRUE)
+
+  return(list("condition 1" = gam_1, "condition 2" = gam_2, "combined conditions" = gam_comb))
 }
