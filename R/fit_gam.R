e92a5571bcbcba21169317e5830d123bcaf6e75f
mmolski <mikolajm2001@gmail.com>
2025-05-15 14:30:25 +0200

    Quick changes

--- R/fit_gam.R
+++ R/fit_gam.R
@@ -7,11 +7,11 @@
 #'
 #' @returns A model
 #' @export
-fit_gam <- \(data_frame, smoothing_basis = "tp", number_of_knots = 10, method = "REML"){
+fit_gam <- \(data_frame, smoothing_basis = "tp", number_of_knots = 10, estimating_method = "REML"){
 
-  g <- gam(fMRI_values ~ s(Time_to_Repetition, bs = bs_options[i], k = 10, by = condition)
-      + s(base_values, bs = bs_options[i], k = 10, by = condition)
+  g <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots, by = condition)
+      + s(base_values, bs = bs_options[i], k = number_of_knots, by = condition)
       + ti(Time_to_Repetition, base_values) + condition,
-      data = long_data_combined, method = "REML", select = TRUE)
+      data = data_frame, method = estimating_method, select = TRUE)
   return(g)
 }
---
+++ tests/testthat/test-fit_gam.R
@@ -0,0 +1,5 @@
+# How can I test this without a data set?
+
+test_that("fit_gam() fits a GAM", {
+  expect_equal(is(fit_gam()), )
+})
