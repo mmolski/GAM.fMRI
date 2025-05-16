b26175baf948eb6a366c51469fc694b2fa3444d0
mmolski <mikolajm2001@gmail.com>
2025-05-16 15:58:26 +0200

    Adding no condition gam model

--- R/fit_gam.R
+++ R/fit_gam.R
@@ -11,18 +11,61 @@
 #' @returns List of 3 GAM models, one for the first condition, one for second, and one for combined case scenario.
 #' @export
 #'
-fit_gam <- \(data_con_1, data_con_2, data_comb, smoothing_basis = "tp", number_of_knots = 10, estimating_method = "REML"){
+fit_gam <- \(
+  data_con_1,
+  data_con_2,
+  data_comb,
+  smoothing_basis = "tp",
+  number_of_knots = 10,
+  estimating_method = "REML"
+) {
+  gam_1 <- gam(
+    fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
+    + ti(Time_to_Repetition, base_values),
+    data = data_con_1,
+    method = estimating_method,
+    select = TRUE
+  )
 
-  gam_1 <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
-      + ti(Time_to_Repetition, base_values), data = data_con_1, method = estimating_method, select = TRUE)
+  gam_2 <- gam(
+    fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
+    + ti(Time_to_Repetition, base_values),
+    data = data_con_2,
+    method = estimating_method,
+    select = TRUE
+  )
 
-  gam_2 <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots) + s(base_values, bs = smoothing_basis, k = number_of_knots)
-      + ti(Time_to_Repetition, base_values), data = data_con_2, method = estimating_method, select = TRUE)
+  gam_comb_no_cond <- gam(
+    fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots)
+    + s(base_values, bs = smoothing_basis, k = number_of_knots)
+    + ti(Time_to_Repetition, base_values),
+    data = data_comb,
+    method = estimating_method,
+    select = TRUE
+  )
 
-  gam_comb <- gam(fMRI_values ~ s(Time_to_Repetition, bs = smoothing_basis, k = number_of_knots, by = condition)
-      + s(base_values, bs = smoothing_basis, k = number_of_knots, by = condition)
-      + ti(Time_to_Repetition, base_values) + condition,
-      data = data_comb, method = estimating_method, select = TRUE)
+  gam_comb_cond <- gam(
+    fMRI_values ~ s(
+      Time_to_Repetition,
+      bs = smoothing_basis,
+      k = number_of_knots,
+      by = condition
+    )
+    + s(
+      base_values,
+      bs = smoothing_basis,
+      k = number_of_knots,
+      by = condition
+    )
+    + ti(Time_to_Repetition, base_values) + condition,
+    data = data_comb,
+    method = estimating_method,
+    select = TRUE
+  )
 
-  return(list("condition 1" = gam_1, "condition 2" = gam_2, "combined conditions" = gam_comb))
+  return(list(
+    "condition 1" = gam_1,
+    "condition 2" = gam_2,
+    "combined conditions" = gam_comb
+  ))
 }
