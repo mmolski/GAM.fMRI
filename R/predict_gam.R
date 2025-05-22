b51e67f786a196b42ffc7756fad657a45a0b19ec
mmolski <mikolajm2001@gmail.com>
2025-05-22 13:22:44 +0200

    Adding conditions

--- R/predict_gam.R
+++ R/predict_gam.R
@@ -10,7 +10,6 @@
 #' @param gam_comb_no_cond Model which does not include condition as a factor
 #' @param no_of_inter Number of interpolated values
 #' @param CI Confidence interval for the plot drawing
-
 #'
 #' @returns A matrix with predictions for both models.
 #' @export
@@ -28,8 +27,19 @@ predict_gam <- \(
   ) {
 
   num_TR_post <- max(data_comb$Time_to_Repetition)
-  predict_matrix <- matrix(nrow = no_of_inter * nrow(data_comb), ncol = 2)
   intrapolation_seq <- seq(1, num_TR_post, length = no_of_inter)
+  predict_matrix_comb <- matrix(nrow = no_of_inter * nrow(data_comb), ncol = 2)
+  predict_matrix_con <- matrix(nrow = no_of_inter * nrow(data_con_1), ncol = 2)
+
+  df_interpolated_c1 <- data.frame(
+    Time_to_Repetition = rep(intrapolation_seq, nrow(data_con_1) / 10),
+    base_values = rep(data_con_1$base_values, each = no_of_inter)
+  )
+
+  df_interpolated_c2 <- data.frame(
+    Time_to_Repetition = rep(intrapolation_seq, nrow(data_con_2) / 10),
+    base_values = rep(data_con_2$base_values, each = no_of_inter)
+  )
 
   df_interpolated_combined <- data.frame(
     Time_to_Repetition = rep(intrapolation_seq, nrow(data_comb) / 10),
@@ -37,8 +47,17 @@ predict_gam <- \(
     condition = c(rep(1, no_of_inter * nrow(data_comb) / 2), rep(2, no_of_inter * nrow(data_comb) / 2))
   )
 
-  predict(gam_comb_cond, newdata = df_interpolated_combined) -> predict_matrix[, 1]
-  predict(gam_comb_no_cond, newdata = df_interpolated_combined) -> predict_matrix[, 2]
+
+  predict(gam_con_1, newdata = df_interpolated_c1) -> predict_matrix_con[, 1]
+  predict(gam_con_2, newdata = df_interpolated_c2) -> predict_matrix_con[, 2]
+  predict(gam_comb_cond, newdata = df_interpolated_combined) -> predict_matrix_comb[, 1]
+  predict(gam_comb_no_cond, newdata = df_interpolated_combined) -> predict_matrix_comb[, 2]
+
+  Avg_pred_gams_con_1 <- numeric(length = no_of_inter)
+  Var_pred_gams_con_1 <- numeric(length = no_of_inter)
+
+  Avg_pred_gams_con_2 <- numeric(length = no_of_inter)
+  Var_pred_gams_con_2 <- numeric(length = no_of_inter)
 
   Avg_pred_gams_combined_cond <- numeric(length = no_of_inter)
   Var_pred_gams_combined_cond <- numeric(length = no_of_inter)
@@ -47,9 +66,27 @@ predict_gam <- \(
   Var_pred_gams_combined_no_cond <- numeric(length = no_of_inter)
 
   for (i in 1:length(intrapolation_seq)) {
+    temp_avg <- numeric(length = nrow(data_con_1) / 10)
+    for (j in 1:(nrow(data_con_1) / 10)) {
+      temp_avg[j] <- predict_matrix_con[i + (j - 1) * no_of_inter, 1]
+    }
+    Var_pred_gams_con_1[i] <- var(temp_avg)
+    Avg_pred_gams_con_1[i] <- mean(temp_avg)
+  }
+
+  for (i in 1:length(intrapolation_seq)) {
+    temp_avg <- numeric(length = nrow(data_con_2) / 10)
+    for (j in 1:(nrow(data_con_2) / 10)) {
+      temp_avg[j] <- predict_matrix_con[i + (j - 1) * no_of_inter, 2]
+    }
+    Var_pred_gams_con_2[i] <- var(temp_avg)
+    Avg_pred_gams_con_2[i] <- mean(temp_avg)
+  }
+
+  for (i in 1:length(intrapolation_seq)) {
     temp_avg <- numeric(length = nrow(data_comb) / 10)
     for (j in 1:(nrow(data_comb) / 10)) {
-      temp_avg[j] <- predict_matrix[i + (j - 1) * no_of_inter, 1]
+      temp_avg[j] <- predict_matrix_comb[i + (j - 1) * no_of_inter, 1]
     }
     Var_pred_gams_combined_cond[i] <- var(temp_avg)
     Avg_pred_gams_combined_cond[i] <- mean(temp_avg)
@@ -58,11 +95,17 @@ predict_gam <- \(
   for (i in 1:length(intrapolation_seq)) {
     temp_avg <- numeric(length = nrow(data_comb) / 10)
     for (j in 1:(nrow(data_comb) / 10)) {
-      temp_avg[j] <- predict_matrix[i + (j - 1) * no_of_inter, 2]
+      temp_avg[j] <- predict_matrix_comb[i + (j - 1) * no_of_inter, 2]
     }
     Var_pred_gams_combined_no_cond[i] <- var(temp_avg)
     Avg_pred_gams_combined_no_cond[i] <- mean(temp_avg)
   }
+
+  SD_pred_gams_con_1 <- sqrt(Var_pred_gams_con_1)
+  SD_pred_gams_con_2 <- sqrt(Var_pred_gams_con_2)
+
+  SE_pred_gams_con_1 <- SD_pred_gams_con_1 / sqrt(length(SD_pred_gams_con_1))
+  SE_pred_gams_con_2 <- SD_pred_gams_con_2 / sqrt(length(SD_pred_gams_con_2))
 
   SD_pred_gams_combined_cond <- sqrt(Var_pred_gams_combined_cond)
   SD_pred_gams_combined_no_cond <- sqrt(Var_pred_gams_combined_no_cond)
@@ -72,14 +115,24 @@ predict_gam <- \(
 
   CI_val <- qnorm(1 - CI / 2)
 
+  Upper_ci_con_1 <- Avg_pred_gams_con_1 + CI_val * SE_pred_gams_con_1
+  Lower_ci_con_1 <- Avg_pred_gams_con_1 - CI_val * SE_pred_gams_con_1
+
+  Upper_ci_con_2 <- Avg_pred_gams_con_2 + CI_val * SE_pred_gams_con_2
+  Lower_ci_con_2 <- Avg_pred_gams_con_2 - CI_val * SE_pred_gams_con_2
+
   Upper_ci_combined_cond <- Avg_pred_gams_combined_cond + CI_val * SE_pred_gams_combined_cond
   Lower_ci_combined_cond <- Avg_pred_gams_combined_cond - CI_val * SE_pred_gams_combined_cond
 
   Upper_ci_combined_no_cond <- Avg_pred_gams_combined_no_cond + CI_val * SE_pred_gams_combined_no_cond
   Lower_ci_combined_no_cond <- Avg_pred_gams_combined_no_cond - CI_val * SE_pred_gams_combined_no_cond
 
-  nonoverlap_index <- which(Lower_ci_combined_cond < Upper_ci_combined_no_cond |
-                            Lower_ci_combined_no_cond < Upper_ci_combined_cond)
+  nonoverlap_index_con <- which(Lower_ci_con_1 < Upper_ci_con_2 |
+                                  Lower_ci_con_2 < Upper_ci_con_1)
+
+  nonoverlap_index_comb <- which(Lower_ci_combined_cond < Upper_ci_combined_no_cond |
+                                   Lower_ci_combined_no_cond < Upper_ci_combined_cond)
+
   # Plotting predictions
 
   df_pred_plot <- data.frame(
@@ -90,7 +143,7 @@ predict_gam <- \(
     lower_cond = Lower_ci_combined_cond,
     upper_no_cond = Upper_ci_combined_no_cond,
     lower_no_cond = Lower_ci_combined_no_cond,
-    nonoverlap_TRs = intrapolation_seq[nonoverlap_index]
+    nonoverlap_TRs_comb = intrapolation_seq[nonoverlap_index_comb]
   )
 
   pred_plot <- ggplot(df_pred_plot, aes(x = TR_vec)) +
@@ -128,7 +181,7 @@ predict_gam <- \(
     ) +
 
     geom_rug(
-      aes(x = nonoverlap_TRs),
+      aes(x = nonoverlap_TRs_comb),
       sides = "b",
       inherit.aes = TRUE,
       color = "black",
