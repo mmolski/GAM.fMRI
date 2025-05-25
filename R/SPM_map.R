3f7e74785af9212648a641084e5e56be988c491d
mmolski <mikolajm2001@gmail.com>
2025-05-25 21:06:41 +0200

    Preparation finished

--- R/SPM_map.R
+++ R/SPM_map.R
@@ -1,12 +1,13 @@
 SPM_map <- \(data_gz_val,  x, y, z) {
 
-
-
+  #
   xdim = dim(data_gz_val$data)[1]
   ydim = dim(data_gz_val$data)[2]
   zdim = dim(data_gz_val$data)[3]
 
   layout(matrix(1:4, nrow=2, byrow=T))
+
+
   image(1:ydim, 1:zdim, data_gz_val$data[x,,])
   points(y, z, pch = 16, col = 'black')
   image(1:xdim, 1:zdim, data_gz_val$data[,y,])
@@ -14,30 +15,34 @@ SPM_map <- \(data_gz_val,  x, y, z) {
   image(1:xdim, 1:ydim, data_gz_val$data[,,z])
   points(x, y, pch = 16, col = 'black')
 
-  # Try
 
-  library(ggplot2)
-
-  library(reshape2)
-
-  # Convert matrix to long format
-
-  df <- melt(mat)
-
-  names(df) <- c("Y", "X", "value")  # match coordinate names
-
-  # Flip Y axis so it looks like base R image()
-
-  ggplot(df, aes(x = X, y = Y, fill = value)) +
-
-    geom_raster() +
-
-    scale_y_reverse() +  # to mimic base R's image() orientation
-
-    scale_fill_gradientn(colors = terrain.colors(10)) +
-
-    theme_minimal()
-
-  mat <- matrix(1:100, nrow = 10)
 
 }
+
+
+
+# Try
+
+library(ggplot2)
+
+library(reshape2)
+
+# Convert matrix to long format
+
+df <- melt(mat)
+
+names(df) <- c("Y", "X", "value")  # match coordinate names
+
+# Flip Y axis so it looks like base R image()
+
+ggplot(df, aes(x = X, y = Y, fill = value)) +
+
+  geom_raster() +
+
+  scale_y_reverse() +  # to mimic base R's image() orientation
+
+  scale_fill_gradientn(colors = terrain.colors(10)) +
+
+  theme_minimal()
+
+mat <- matrix(1:100, nrow = 10)
--- R/SPM_preparation.R
+++ R/SPM_preparation.R
@@ -1,4 +1,13 @@
-SPM_preparation <- \(data_gz, lrt_array) {
+#' Preparation for SPM map
+#'
+#' @param data_gz Main data file (.gz extension) with BOLD signal values.
+#' @param lrt_array The array of p-values of LRT evaluated on model with condition included vs the one without.
+#' @param dfs_model_comp The difference of degrees of freedom between models.
+#'
+#' @returns Two data overwritten objects. First with p-values and the other with z-values.
+#' @export
+#'
+SPM_preparation <- \(data_gz, lrt_array, dfs_model_comp = 5) {
 
   # Setting the data to the array in nifiti file
   data_gz$setData(lrt_array)
@@ -12,9 +21,18 @@ SPM_preparation <- \(data_gz, lrt_array) {
   # Transforming to Z-values
   data_gz_val <- data_gz$clone()
 
-  df <- 5 # degrees of freedom
+  df <- dfs_model_comp
+  qchisq(0.05, df = 5, lower.tail = FALSE)
 
-  lrt_z
+  lrt_z <- lrt_array
+
+  for (i in 1:dim(lrt_z)[1]) {
+    for (j in 1:dim(lrt_z)[2]) {
+      for (k in 1:dim(lrt_z)[3]) {
+        lrt_z[i,j,k] <- qchisq(lrt_array[i,j,k], df = 5, lower.tail = FALSE)
+          }
+        }
+      }
 
   data_gz_val$setData(lrt_z)
   data_gz_val$changeName('LRT_zvals')
